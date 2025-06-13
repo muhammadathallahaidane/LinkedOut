12bddf4606e1723d228138f45994a33a633e3853
muhammadathallahaidane <talaaidane@gmail.com>
2025-06-13 11:05:11 +0700

    feat: integrate Apollo Client, add Home screen navigation, implement login feature

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,11 +1,58 @@
+import { gql, useLazyQuery } from "@apollo/client";
 import { useNavigation } from "@react-navigation/native";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import * as SecureStore from "expo-secure-store";
+
+const LOGIN = gql`
+  query Login($username: String, $password: String) {
+    login(username: $username, password: $password)
+  }
+`;
 
 export default function LoginScreen() {
   const navigation = useNavigation();
+  const [input, setInput] = useState({
+    username: "",
+    password: "",
+  });
+  const [loginQuery, { loading, data, error }] = useLazyQuery(LOGIN, {
+    onCompleted: async function (result) {
+      await SecureStore.setItemAsync("access_token", result.login);
+    },
+  });
+
+  if (loading)
+    return (
+      <View>
+        <Text>loading...</Text>
+      </View>
+    );
+  if (error)
+    return (
+      <View>
+        <Text>error... {error.message}</Text>
+      </View>
+    );
+
+  function handleLogin() {
+    loginQuery({
+      variables: {
+        username: input.username,
+        password: input.password
+      }
+    })
+  }
+
+  async function tokenCheck() {
+    const token = await SecureStore.getItemAsync("access_token")
+    console.log(token);
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Ini Login Screen</Text>
+      <Text>Login Screen</Text>
       <Button
         title="Register"
         onPress={() => navigation.navigate("RegisterScreen")}
@@ -14,16 +61,59 @@ export default function LoginScreen() {
         title="Home"
         onPress={() => navigation.navigate("HomeNavigator")}
       />
+      <Button
+        title="Cek Token"
+        onPress={tokenCheck}
+      />
+      <Text>{JSON.stringify(input, null, 2)}</Text>
+      <Text>Username</Text>
+      <TextInput
+        style={styles.input}
+        value={input.username}
+        onChangeText={(text) =>
+          setInput({
+            ...input,
+            username: text,
+          })
+        }
+      />
+      <Text>Password</Text>
+      <TextInput
+        style={styles.input}
+        value={input.password}
+        onChangeText={(text) =>
+          setInput({
+            ...input,
+            password: text,
+          })
+        }
+      />
+      <View style={{ width: 200, marginTop: 100 }}>
+        <Button
+          title="Login"
+          onPress={handleLogin}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "teal",
-
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "teal",
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 10,
+    width: 350,
+    padding: 10,
+    margin: 5,
+    backgroundColor: "#fff",
+  },
+  button: {
+    width: 200,
+  },
+});
--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function RegisterScreen() {
   const navigation = useNavigation();
@@ -10,16 +10,31 @@ export default function RegisterScreen() {
         title="Login"
         onPress={() => navigation.navigate("LoginScreen")}
       />
+      <Text>Username</Text>
+      <TextInput style={styles.input} />
+      <Text>Password</Text>
+      <TextInput style={styles.input} />
+      <Button
+        title="Register"
+        onPress={() => navigation.navigate("HomeNavigator")}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "teal",
-
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "teal",
+  },
+    input: {
+    borderWidth: 1,
+    borderRadius: 10,
+    width: 350,
+    padding: 10,
+    margin: 10,
+    backgroundColor: "#fff",
+  },
+});
